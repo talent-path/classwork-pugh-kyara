6be1ba48c9846a5f234163dc06efe6b71b3ce8ef
Kyara Pugh <kyarapugh@C02DQ8HSML7H.local>
2021-01-15 11:04:54 -0500

    added a continue feature to game

--- TicTacToe/src/main/java/Game.java
+++ TicTacToe/src/main/java/Game.java
@@ -9,102 +9,120 @@ public class Game {
     static String yourToken = "";
     static String compToken = "";
     static Random rand = new Random();
-    //static String [] gameArray = new String[9];
     static String [] gameArray;
 
+    //main method
     public static void main(String[] args) {
-        //printGame();
-        //gameWon();
+
         Play();
     }
 
     //main playing method
     public static void Play() {
+        int quit = 0;
+        // do loop allows player to choose whether they want to play again
+        do {
         Scanner input = new Scanner(System.in);
         System.out.println("How many rounds will you play?");
         int wins = 0;
         int rounds = 1;
         int setRounds = numOfRounds(input);
-        if (setRounds > 0) {
-            System.out.println("Flipping to determine your token...");
-            boolean side = coinFlip();
 
-            //keep going for number of rounds given
-            while (rounds <= setRounds) {
-                System.out.println("Round " + rounds);
-                printSide(side);
-                gameArray = new String[]{" ", " ", " ", " ", " ", " ", " ", " ", " "};
-                int turns = 1;
-                boolean gameOver = false;
-                String winner = null;
-                while (!gameOver && winner == null) {
-                    if (side) {
-                        //asks players to choose a spot and inputs their token
-                        updateGame(chooseSpot(input), yourToken);
-                        printGame();
-                        if (gameOver(matchUpdate()) || turns > 9) {
-                            gameOver = true;
-                            if (yourToken.equals(matchUpdate())) {
-                                winner = "Player";
-                                System.out.println("Match over! " + winner + " is the winner!");
-                                rounds++;
-                                wins++;
-                            } else if (compToken.equals(matchUpdate())) {
-                                winner = "Computer";
-                                System.out.println("Match over! " + winner + " is the winner!");
-                                rounds++;
+            //if the player decides to do more than 0 rounds
+            if (setRounds > 0) {
+                System.out.println("Flipping to determine your token...");
+                boolean side = coinFlip();
+
+                //keep going for number of rounds given
+                while (rounds <= setRounds) {
+                    System.out.println("Round " + rounds);
+                    printSide(side);
+                    gameArray = new String[]{" ", " ", " ", " ", " ", " ", " ", " ", " "};
+                    int turns = 1;
+                    boolean gameOver = false;
+                    String winner = null;
+                    while (!gameOver && winner == null) {
+
+                        //player's turn
+                        if (side) {
+                            //asks players to choose a spot and inputs their token
+                            updateGame(chooseSpot(input), yourToken);
+                            printGame();
+                            if (gameOver(matchUpdate()) || turns > 9) {
+                                gameOver = true;
+                                if (yourToken.equals(matchUpdate())) {
+                                    winner = "Player";
+                                    System.out.println("Match over! " + winner + " is the winner!");
+                                    rounds++;
+                                    wins++;
+                                } else if (compToken.equals(matchUpdate())) {
+                                    winner = "Computer";
+                                    System.out.println("Match over! " + winner + " is the winner!");
+                                    rounds++;
+                                }
+
                             }
-
-                        }
-                        //switch sides
-                        else {
-                            side = false;
-                            turns++;
-                        }
-                    }
-
-                    else if (!side) {
-                        updateGame(generateSpot(), compToken);
-                        printGame();
-                        //check if the game is over or if 9 turns have been reached
-                        if (gameOver(matchUpdate()) || turns > 9) {
-                            gameOver = true;
-                            if (yourToken.equals(matchUpdate())) {
-                                winner = "Player";
-                                System.out.println("Match over! " + winner + " is the winner!");
-                                rounds++;
-                                wins++;
-                            } else if (compToken.equals(matchUpdate())) {
-                                winner = "Computer";
-                                System.out.println("Match over! " + winner + " is the winner!");
-                                rounds++;
+                            //switch sides
+                            else {
+                                side = false;
+                                turns++;
                             }
                         }
-                        //switch sides
-                        else {
-                            side = true;
-                            turns++;
+                        //computer's turn
+                        else if (!side) {
+                            updateGame(generateSpot(), compToken);
+                            printGame();
+                            //check if the game is over or if 9 turns have been reached
+                            if (gameOver(matchUpdate()) || turns > 9) {
+                                gameOver = true;
+                                if (yourToken.equals(matchUpdate())) {
+                                    winner = "Player";
+                                    System.out.println("Match over! " + winner + " is the winner!");
+                                    rounds++;
+                                    wins++;
+                                } else if (compToken.equals(matchUpdate())) {
+                                    winner = "Computer";
+                                    System.out.println("Match over! " + winner + " is the winner!");
+                                    rounds++;
+                                }
+                            }
+                            //switch sides
+                            else {
+                                side = true;
+                                turns++;
+                            }
                         }
-                    }
-                    if(turns > 9 && winner == null)
-                    {
-                        gameOver = true;
-                        System.out.println("Match over! Draw!");
-                        rounds++;
+                        //if no one wins in more than 9 turns declare a draw
+                        if (turns > 9 && winner == null) {
+                            gameOver = true;
+                            System.out.println("Match over! Draw!");
+                            rounds++;
+                        }
+
                     }
 
                 }
-
+                //output wins
+                System.out.println("You had " + wins + " win(s).");
             }
-            System.out.println("You had " + wins + " win(s).");
-        }
-        else
-        {
-            System.out.println("That's too bad. Goodbye!");
-        }
+            //outout dialog for if the player decides to play no rounds at all
+            else {
+                System.out.println("That's too bad. Goodbye!");
+                //leave loop
+                break;
+            }
+            System.out.println("Do you want to play again? Yes(0), No(1)");
+            quit = getInt(input);
+            while(quit != 1 && quit!=0)
+            {
+                System.out.println("Invalid choice try again.");
+                quit = getInt(input);
+            }
+        } while(quit!=1); //end of do loop
+        System.out.println("Thanks for playing!");
     }
 
-    //determines sides
+    //determines sides and their token
     public static void printSide(boolean side)
     {
         if (side) {
@@ -120,6 +138,7 @@ public class Game {
         }
     }
 
+    //print the gameboard
     public static void printGame()
     {
         System.out.println(" " + gameArray[0] + " | " + gameArray[1]+ " | "+ gameArray[2]);
@@ -171,6 +190,7 @@ public class Game {
                     break;
             }
 
+            //output the winner token
             if (line.equals("XXX")) {
                 winner = "X";
             } else if (line.equals("OOO")) {
@@ -180,6 +200,7 @@ public class Game {
       return winner;
     }
 
+    //output if the game is over because someone has won and their token
     public static boolean gameOver(String m)
     {
         boolean over = false;
@@ -209,6 +230,7 @@ public class Game {
 
 
     //user choose the spot they would like
+    //cannot be on an occupied spot
     public static int chooseSpot(Scanner input)
     {
         System.out.println("Your turn to pick...");
@@ -241,12 +263,14 @@ public class Game {
     //check whether a spot is already filled
     public static boolean isOccupied(String [] a, int spot)
     {
+
         return (a[spot].equals(X) || a[spot].equals(O));
     }
 
     //decide whether player will be X or O
     public static boolean coinFlip()
     {
+
         return rand.nextBoolean();
     }
 
