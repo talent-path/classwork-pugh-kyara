dea556d777e26267f1c07a5a7f732c942b6390e7
Kyara Pugh <kyarapugh@C02DQ8HSML7H.local>
2021-01-20 17:57:28 -0500

    pokerhands commit

---
+++ PokerHands/src/main/java/Application.java
@@ -0,0 +1,131 @@
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Application {
+    public static void main(String[] args) {
+
+        //define a variable of the TYPE Suit
+        //Suit cardSuit = Suit.DIAMONDS;
+        //FaceValue cardValue = FaceValue.ACE;
+
+        //we're using the new keyword to "instantiate" a PlayingCard object
+        //when we use the new keyword, it calls the constructor method
+        //we haven't defined a constructor, so it just calls the automatic default that does nothing
+        //we get back a reference to the object (which is on the heap)
+        //that reference is what's stored inside of aceOfDiamonds
+        //PlayingCard aceOfDiamonds = new PlayingCard();
+
+        //this no longer works now that we have a non-default constructor, the default goes away
+        //now we have to instantiate with the data already provided
+        //the advantage is that it is impossible to build a playing card object
+        //without supplying all the of information necessary to describe the card
+        //Card aceOfDiamonds = new Card( FaceValue.ACE, cardSuit );
+
+        //so now we can call non-static methods
+        //NOT on PlayingCard, but instead on aceOfDiamonds
+        //aceOfDiamonds.setCardValue( FaceValue.ACE );
+        //aceOfDiamonds.setCardSuit( Suit.DIAMONDS );
+
+        //System.out.println( aceOfDiamonds.getCardValue() );
+        //System.out.println( aceOfDiamonds.getCardSuit() );
+
+        //this doesn't work because these methods are not static:
+//        PlayingCard.setCardValue( FaceValue.ACE );
+//        PlayingCard.setCardSuit( Suit.DIAMONDS );
+
+
+        //                   straight flush vs 4 of a kind
+        //                   2D  3D 4D 5D  6D  7S  7D  7C  7H  8S
+        int[] bothHands = { 1, 5, 9, 13, 17, 20, 21, 22, 23, 24  };
+        Card[] hand1Cards = new Card[5];
+        Card[] hand2Cards = new Card[5];
+
+        for( int i = 0; i < bothHands.length; i++ ){
+            if( i < 5 ){
+                hand1Cards[i] = new Card(bothHands[i]);
+            } else {
+                hand2Cards[i-5] = new Card(bothHands[i]);
+            }
+        }
+
+        PokerHand hand1 = new PokerHand(hand1Cards);
+        PokerHand hand2 = new PokerHand(hand2Cards);
+
+        //testing value outputs to delete later
+//        System.out.println(hand1.isFlush());
+//        System.out.println(hand2.isFlush());
+        System.out.println(hand1.fourOfAKindValue());
+        System.out.println(hand2.fourOfAKindValue());
+
+        //test suit and face value counts
+        Map<FaceValue,Integer> valueCountsH1 = hand1.countFaceValues();
+        Map<FaceValue,Integer> valueCountsH2 = hand2.countFaceValues();
+
+        Map<Suit, Integer> suitCountsH1 = hand1.countSuits();
+        Map<Suit, Integer> suitCountsH2 = hand2.countSuits();
+
+        System.out.println("hand 1: ");
+        for( FaceValue key : valueCountsH1.keySet() ){
+            System.out.println( key + ": " + valueCountsH1.get(key) );
+        }
+
+        for( Suit key : suitCountsH1.keySet() ){
+            System.out.println( key + ": " + suitCountsH1.get(key) );
+        }
+
+        System.out.println("hand 2: ");
+        for( FaceValue key : valueCountsH2.keySet() ){
+            System.out.println( key + ": " + valueCountsH2.get(key) );
+        }
+
+        for( Suit key : suitCountsH2.keySet() ){
+            System.out.println( key + ": " + suitCountsH2.get(key) );
+        }
+
+
+        int hand1WinsResult = hand1.compareTo(hand2);
+
+        if( hand1WinsResult < 0 ){
+            //should see this
+            System.out.println("player 1 wins");
+        } else if( hand1WinsResult > 0 ){
+            System.out.println( "player 2 wins");
+        } else {
+            System.out.println("tie!");
+        }
+
+    }
+
+    //player one's cards are 0-4
+    //player two's cards are 5-9
+    public static boolean playerOneWins( int[] bothHands ){
+        throw new UnsupportedOperationException();
+    }
+
+    //generates an array of size 10 representing 2 hands of poker
+    //the values are 0-51
+    //the suit is value%4            (0-spades, 1-diamonds, 2-clubs, 3-hearts)
+    //the card is value/4 + 2       J = 11, Q = 12, K = 13, A = 14
+    public static int[] generatePokerHands(){
+        int [] cards = new int[10];
+        for( int i = 0; i < 10; i++ ){
+            int card = Integer.MIN_VALUE;
+
+            while( card == Integer.MIN_VALUE ){
+                card = Rng.nextInt(0,51);
+                for( int j = 0; j < i; j++ ){
+                    if( cards[j] == card ) card = Integer.MIN_VALUE;
+                }
+
+            }
+            cards[i] = card;
+        }
+
+        Arrays.sort(cards, 0, 4);
+        Arrays.sort(cards, 5, 9);
+
+        return cards;
+    }
+
+}
---
+++ PokerHands/src/main/java/PokerHand.java
@@ -0,0 +1,198 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class PokerHand {
+    //field variables
+    //we'll "compose" a poker hand from multiple sub-objects that we pull into a collection
+    private Card[] cards;
+
+    //need a constructor to make sure every hand is built with all the cards needed
+    public PokerHand( Card[] cards ) {
+        //for now we'll cross our fingers and hope its always 5
+//        if( cards.length < 5 ){
+//            //TODO: create a specific exception for this
+//            throw new Exception("Invalid number of cards.");
+//        }
+
+
+        this.cards = cards;
+
+        sortCards();
+    }
+
+    private void sortCards() {
+
+        //bubble sort
+        boolean sorted = false;
+
+        while( !sorted ){
+
+            sorted = true;
+            for( int i = 0; i < cards.length - 1; i++){
+                //does the element at i need to be flipped with the element at i + 1?
+                if( (cards[i].getCardValue().value < cards[i+1].getCardValue().value) ||
+
+                        (cards[i].getCardValue().value == cards[i+1].getCardValue().value
+                        && cards[i].getCardSuit().value < cards[i+1].getCardSuit().value
+                        )
+                ){
+                    //we need to swap elements at i and i + 1
+                    //it also means we found elements out of order
+                    sorted = false;
+                    Card temp = cards[i+1];
+                    cards[i+1] = cards[i];
+                    cards[i] = temp;
+                }
+            }
+
+        }
+
+    }
+
+    public Card[] getCards(){
+        return cards;
+    }
+
+    //counts the number of values in a hand using maps
+    public Map<FaceValue, Integer> countFaceValues(){
+
+        Map<FaceValue, Integer> count = new HashMap<>();
+
+        for( Card toCount : cards ){
+            if( !count.containsKey(toCount.getCardValue())){
+                count.put(toCount.getCardValue(), 0);
+            }
+
+            Integer currentCount = count.get(toCount.getCardValue());
+
+            count.put( toCount.getCardValue(),
+                    1 + currentCount );
+
+        }
+
+        return count;
+    }
+
+    //counts the number of suits in a hand using maps
+    public Map<Suit, Integer> countSuits(){
+
+        Map<Suit, Integer> count = new HashMap<>();
+
+        for( Card toCount : cards ){
+            if( !count.containsKey(toCount.getCardSuit())){
+                count.put(toCount.getCardSuit(), 0);
+            }
+
+            Integer currentCount = count.get(toCount.getCardSuit());
+
+            count.put( toCount.getCardSuit(),
+                    1 + currentCount );
+
+        }
+
+        return count;
+    }
+
+
+    //if no straight, return a null
+    public FaceValue straightHighCardValue(){
+
+        for (int i = 0; i < cards.length-1; i++) {
+            if(cards[i].getCardValue().value != cards[i+1].getCardValue().value+1)
+            {
+                return null;
+            }
+        }
+     return cards[0].getCardValue();
+    }
+
+    public boolean isFlush(){
+        Map<Suit, Integer> suits = countSuits();
+        Suit suit = this.cards[0].getCardSuit();
+        boolean flush = false;
+        for(Suit key : suits.keySet())
+        {
+            if(key == suit) {
+                flush = true;
+                System.out.println("From the isFlush method, The key is: " + key);
+            }
+            else
+            {
+                flush = false;
+            }
+        }
+        return flush;
+    }
+
+
+    public boolean isStraightFlush(){
+
+        return (straightHighCardValue() != null) && isFlush();
+    }
+
+    public boolean isFullHouse(){
+
+        return pairValue() != null && threeOfAKindValue() != null;
+    }
+
+    public boolean isRoyalFlush(){
+
+        return isStraightFlush() && cards[0].getCardValue() == FaceValue.ACE;
+    }
+
+    //if not 4 of a kind, return null
+    public FaceValue fourOfAKindValue(){
+        Map<FaceValue, Integer> values = countFaceValues();
+        FaceValue cardVal = this.cards[0].getCardValue();
+        for(FaceValue key : values.keySet())
+        {
+            System.out.println("From 4 of a kind method, the key is: " + key);
+            if(key != cardVal)
+            {
+                cardVal = null;
+            }
+            else
+            {
+
+            }
+        }
+        return this.cards[0].getCardValue();
+    }
+
+    //should return null if there are really 4
+    public FaceValue threeOfAKindValue(){
+        Map<FaceValue, Integer> suits = countFaceValues();
+
+        throw new UnsupportedOperationException();
+    }
+
+    //should return null if there are really 3 (or more of the same card)
+    public FaceValue pairValue(){
+        Map<FaceValue, Integer> suits = countFaceValues();
+        throw new UnsupportedOperationException();
+    }
+
+    //should return null when there is only one pair
+    public FaceValue lowerPairValue(){
+        throw new UnsupportedOperationException();
+    }
+
+
+
+
+    //return 0 if "this" ties with "that"
+    //return negative number if "this" wins over "that"
+    //return positive number if "this" loses to "that"
+    public int compareTo( PokerHand that ){
+        int result = 0;
+        return result;
+//        throw new UnsupportedOperationException();
+    }
+
+    //in general compareTo() sematics are
+    // - means "this before that"
+    // 0 means "they're equal"
+    // + means "that before this"
+
+
+}
