0e95b33146088424788b813e650b7cd1a4800d0e
Kyara Pugh <kyarapugh@C02DQ8HSML7H.local>
2021-01-29 17:24:05 -0500

    added potential tests to test suite

--- libraryuserstory/src/test/java/com/tp/libraryuserstory/persistence/LibraryInMemDAOTests.java
+++ libraryuserstory/src/test/java/com/tp/libraryuserstory/persistence/LibraryInMemDAOTests.java
@@ -1,4 +1,55 @@
 package com.tp.libraryuserstory.persistence;
 
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
 public class LibraryInMemDAOTests {
+
+    @Autowired
+    LibraryInMemDAO testDAO;
+
+
+    //delete all books made and create 1 new one
+    @BeforeEach
+    public void setup()
+    {
+
+    }
+
+
+    //creation of a book with no issues
+    @Test
+    public void createBookGood()
+    {
+
+    }
+
+    //Creation of a book with a null ID
+    //goal is to get a InvalidBookIDException
+    @Test
+    public void createBookNullID()
+    {
+
+    }
+
+    //Creation of a book with a null title
+    //goal is to get a NullTitleException
+    @Test
+    public void createBookNullTitle()
+    {
+
+    }
+
+    //Creation of a book with a null ID
+    //goal is to get a NullAuthorException
+    @Test
+    public void createBookNullAuthor()
+    {
+
+    }
 }
+
