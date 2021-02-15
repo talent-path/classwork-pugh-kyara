04526b32944983453baef60652c966bcb142413c
Kyara Pugh <kyarapugh@C02DQ8HSML7H.local>
2021-02-15 11:41:54 -0500

    added to Review tests

--- gamemanagementsystem/src/test/java/com/tp/gamemanagementsystem/daos/ReviewPostgresDAOTests.java
+++ gamemanagementsystem/src/test/java/com/tp/gamemanagementsystem/daos/ReviewPostgresDAOTests.java
@@ -1,6 +1,12 @@
 package com.tp.gamemanagementsystem.daos;
 
+import static org.junit.jupiter.api.Assertions.*;
+
+import com.tp.gamemanagementsystem.exceptions.NullIDException;
+import com.tp.gamemanagementsystem.exceptions.NullReviewException;
+import com.tp.gamemanagementsystem.exceptions.NullTitleException;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.jdbc.core.JdbcTemplate;
@@ -19,7 +25,67 @@ public class ReviewPostgresDAOTests {
     @BeforeEach
     public void setup()
     {
+
+    }
+
+    @Test
+    public void addReviewTest()
+    {
+
+    }
+
+    @Test
+    public void addReviewNullTitle()
+    {
+        assertThrows(NullReviewException.class,()->testDAO.makeReview(null,"My brand new review!",8, 1 ));
+
+    }
+
+    @Test
+    public void addReviewNullReview()
+    {
+        assertThrows(NullReviewException.class,()->testDAO.makeReview("New Review",null,8, 1 ));
+
+    }
+
+    @Test
+    public void addReviewNullGameID()
+    {
+        assertThrows(NullIDException.class,()->testDAO.makeReview("New Review","My brand new review!",8, null ));
+
+    }
+
+    @Test
+    public void editReviewTest()
+    {
         
+    }
+
+    @Test
+    public void editReviewNullID()
+    {
+        assertThrows(NullIDException.class,()->testDAO.editReview(null, "My brand new review!"));
+
+    }
+
+    @Test
+    public void editReviewNullReview()
+    {
+        assertThrows(NullReviewException.class,()->testDAO.editReview(1, null));
+
+    }
+
+    @Test
+    public void deleteReviewTest()
+    {
+
+
+    }
+
+    @Test
+    public void deleteReviewNullID()
+    {
+        assertThrows(NullIDException.class, ()->testDAO.deleteReview(null));
     }
 
 
