a1c83d488889add210f5288f3481231fa214f672
Kyara Pugh <kyarapugh@C02DQ8HSML7H.local>
2021-02-18 01:03:02 -0500

    create a number with unique digits

--- guessingame/app.js
+++ guessingame/app.js
@@ -1,25 +1,56 @@
 let numToGuess = generateNum();
 
-checkGuess()
+function checkGuess(guessedNum)
 {
+    
     //if a number matches and is in the right position display green bg
     //if a number matches and is in the wrong position display yellow bg 
     //if a number doesn't match at all red bg 
 
 }
 
+//fill boxes with the numbers entered by the guesser
+function fillBoxes()
+{
+
+    
+}
+
+//if all the numbers are guessed correctly
+function allGreen()
+{
+
+}
 
 
 //generate a 4 digit number with all unique digits  
 function generateNum()
 {
-   let newNum = Math.floor(Math.random()*(9999-1000))+1000;
-   console.log(newNum);
-   return newNum;
+    let numArr = [null, null, null, null];
+    let newNum = Number.MIN_VALUE;
+    let i = 0
+    //until all nulls are gone
+    while(numArr.includes(null))
+    {
+        //generate a number 1 through 9
+        newNum = Math.floor(Math.random()*8)+1;
+        //add that number if that digit isn't in the array
+        if(!numArr.includes(newNum))
+        {
+            numArr[i]= newNum
+            i++;
+        }
+    }
+    //joins all elements of the array
+    let guessNum = numArr.join('');
+    //changes the string to a number
+    guessNum = parseInt(guessNum);
+    return guessNum;
 }
 
 
+//reset this game
 function reset()
 {
-    generateNum();
+    numToGuess = generateNum();
 }
