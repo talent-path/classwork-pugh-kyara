93e79bb6f15e15f1834ed617c8dcfc8199cb190e
Kyara Pugh <kyarapugh@C02DQ8HSML7H.local>
2021-02-19 18:17:18 -0500

    cleaned up code

--- ffxiv/app.js
+++ ffxiv/app.js
@@ -1,18 +1,17 @@
 
-// character retrieving url (used to get character ID)
+// // character retrieving url (used to get character ID)
 
-const idURL = `https://xivapi.com/character/search?name=${charName}&server=${serverName}&private_key=054354dfd01b48cfb6d88493e6ba759952ec9f0201724b709622e3bad27bbeea`
+// const idURL = `https://xivapi.com/character/search?name=${charName}&server=${serverName}&private_key=054354dfd01b48cfb6d88493e6ba759952ec9f0201724b709622e3bad27bbeea`
 
-// lodestone url; plug in ID from searched character to get more info 
+// // lodestone url; plug in ID from searched character to get more info 
 
-const infoURL = `https://xivapi.com/character/${charID}&private_key=054354dfd01b48cfb6d88493e6ba759952ec9f0201724b709622e3bad27bbeea`
+// const infoURL = `https://xivapi.com/character/${charID}&private_key=054354dfd01b48cfb6d88493e6ba759952ec9f0201724b709622e3bad27bbeea`
 
-//title url
-const titleURL = `https://xivapi.com/title/${titleID}`
+// //title url
+// const titleURL = `https://xivapi.com/title/${titleID}`
 
 //let charID = 18791259;    Daraxi
 //let charID = 31646453;    Avalan
-
 
 
 //return informtation about a specified character
@@ -39,13 +38,28 @@ const getCharacter = function()
             let avaURL = data2.Character.Avatar;
             //big portrait
             let portraitURL = data2.Character.Portrait;
-            //display character title where element ID matches
-            // $("charTitle").text()
 
+            //get player title
+            let titleID = data2.Character.Title;
+            $.get(`https://xivapi.com/title/${titleID}`, function(data3, textStatus, jqXQR)
+            {
+                //display character title where element ID matches
+                if(data3.IsPrefix===1)
+                {
+                    $("#charTitlePrefix").text(data3.Name);
+                    $("#charTitleSuffix").text("");
+                }
+                else if(data3.IsPrefix===0)
+                {
+                    $("#charTitlePrefix").text("");
+                    $("#charTitleSuffix").text(data3.Name);
+
+                }
+            }
+            );
             //put images in respective tags
             $("#charProfile").attr("src",avaURL); 
             $("#charPortrait").attr("src",portraitURL); 
-
             //print data to console
             console.log(data2);
             console.log(textStatus);
@@ -59,61 +73,28 @@ const getCharacter = function()
 }
 
 
-function testWhen()
-{
-    const charName = $("#searchChar").val();
-    const serverName = $("#serverSelect option:selected").val();
-    $.get(`https://xivapi.com/character/search?name=${charName}&server=${serverName}&private_key=054354dfd01b48cfb6d88493e6ba759952ec9f0201724b709622e3bad27bbeea`,function(data,textStatus,jqXQR)
-    {
-
-        console.log(data);
-        $.when($.ajax(infoURL),$.ajax(titleURL)).done(function(a1, a2)
-        {
-            console.log(a1[0]);
-            console.log(a2[0]);
-
-        }) //end of done
+// //gets a title Name by the ID
+// function getTitle(titleID)
+// {
+//     $.get(`https://xivapi.com/title/${titleID}`, function(data,textStatus,jqXQR)
+//     {
+//         if(data.IsPrefix===1)
+//         {
+//             $("#charTitlePrefix").text(data.Name);
+//             $("#charTitleSuffix").text("");
 
 
-        //console.log(data.Results[0].ID)
+//         }
+//         else if(data.IsPrefix===0)
+//         {
+//             $("#charTitlePrefix").text("");
+//             $("#charTitleSuffix").text(data.Name);
 
-        //let charID = a1[0].Results[0].ID;
-
-    }
-    );
-   
-
-  
-    //     console.log(a2[0]);
-    //     console.log(a2[0].Character.Portrait);
-}
-
-// $.ajax(`https://xivapi.com/character/${charID}&private_key=054354dfd01b48cfb6d88493e6ba759952ec9f0201724b709622e3bad27bbeea`)).done(function(a1,a2)
-
-
-
-//gets a title Name by the ID
-function getTitle(titleID)
-{
-    $.get(`https://xivapi.com/title/${titleID}`, function(data,textStatus,jqXQR)
-    {
-        if(data.IsPrefix===1)
-        {
-            $("#charTitlePrefix").text(data.Name);
-            $("#charTitleSuffix").text("");
-
-
-        }
-        else if(data.IsPrefix===0)
-        {
-            $("#charTitlePrefix").text("");
-            $("#charTitleSuffix").text(data.Name);
-
-        }
-        console.log(data.Name);
-    }
-    );
-}
+//         }
+//         console.log(data.Name);
+//     }
+//     );
+// }
 
 
 
