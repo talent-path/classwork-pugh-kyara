4772559c6cb680d421ce221bc22d582b385ed86f
Kyara Pugh <kyarapugh@C02DQ8HSML7H.local>
2021-02-22 11:13:42 -0500

    character search

--- ffxiv/app.js
+++ ffxiv/app.js
@@ -1,14 +1,25 @@
-
+//list of ffxiv servers to choose from
+const servers = ["Adamantoise","Aegis","Alexander","Anima","Asura","Atomos","Bahamut","Balmung","Behemoth","Belias","Brynhildr","Cactuar","Carbuncle","Cerberus","Chocobo","Coeurl","Diabolos","Durandal","Excalibur","Exodus","Faerie","Famfrit","Fenrir","Garuda","Gilgamesh","Goblin","Gungnir","Hades","Hyperion","Ifrit","Ixion","Jenova","Kujata","Lamia","Leviathan","Lich","Louisoix","Malboro","Mandragora","Masamune","Mateus","Midgardsormr","Moogle","Odin","Omega","Pandaemonium","Phoenix","Ragnarok","Ramuh","Ridill","Sargatanas","Shinryu","Shiva","Siren","Tiamat","Titan","Tonberry","Typhon","Ultima","Ultros","Unicorn","Valefor","Yojimbo","Zalera","Zeromus","Zodiark","Spriggan","Twintania","HongYuHai","ShenYiZhiDi","LaNuoXiYa","HuanYingQunDao","MengYaChi","YuZhouHeYin","WoXianXiRan","ChenXiWangZuo","BaiYinXiang","BaiJinHuanXiang","ShenQuanHen","ChaoFengTing","LvRenZhanQiao","FuXiaoZhiJian","Longchaoshendian","MengYuBaoJing","ZiShuiZhanQiao","YanXia","JingYuZhuangYuan","MoDuNa","HaiMaoChaWu","RouFengHaiWan","HuPoYuan"]
+populateList();
 // // character retrieving url (used to get character ID)
-
 // const idURL = `https://xivapi.com/character/search?name=${charName}&server=${serverName}&private_key=054354dfd01b48cfb6d88493e6ba759952ec9f0201724b709622e3bad27bbeea`
 
 // // lodestone url; plug in ID from searched character to get more info 
-
 // const infoURL = `https://xivapi.com/character/${charID}&private_key=054354dfd01b48cfb6d88493e6ba759952ec9f0201724b709622e3bad27bbeea`
 
 // //title url
 // const titleURL = `https://xivapi.com/title/${titleID}`
+
+//job icon url
+// https://xivapi.com/cj/1/[jobname].png
+//alt job icon url
+//https://xivapi.com/cj/companion/[jobname].png
+
+
+//mount image? url
+//https://xivapi.com/[png url]
+//Name 
+//Icon
 
 //let charID = 18791259;    Daraxi
 //let charID = 31646453;    Avalan
@@ -19,30 +30,31 @@ const getCharacter = function()
 {
     const charName = $("#searchChar").val();
     const serverName = $("#serverSelect option:selected").val();
-    //get the ID of the character first
-    $.get(`https://xivapi.com/character/search?name=${charName}&server=${serverName}&private_key=054354dfd01b48cfb6d88493e6ba759952ec9f0201724b709622e3bad27bbeea`,function(data,textStatus,jqXQR)
-    {
-        //print data to console
-            console.log(data);
-            console.log(textStatus);
-            console.log(jqXQR);
-            let charID = data.Results[0].ID;
-        // //search the character ID on the lodestone url
-        $.get(`https://xivapi.com/character/${charID}&private_key=054354dfd01b48cfb6d88493e6ba759952ec9f0201724b709622e3bad27bbeea`,function(data2,textStatus,jqXQR)
+    $.when($.ajax(`https://xivapi.com/character/search?name=${charName}&server=${serverName}&private_key=054354dfd01b48cfb6d88493e6ba759952ec9f0201724b709622e3bad27bbeea`)).then(function(data){
+        let charID = data.Results[0].ID;
+        $.when($.ajax(`https://xivapi.com/character/${charID}&private_key=054354dfd01b48cfb6d88493e6ba759952ec9f0201724b709622e3bad27bbeea`)).then(function(data2)
         {
-            //display character name where element ID matches
-            $("#charName").text(data2.Character.Name);
-            //display server name where element ID matches
-            $("#charServer").text(data2.Character.Server);
-            //mini portrait
-            let avaURL = data2.Character.Avatar;
-            //big portrait
-            let portraitURL = data2.Character.Portrait;
-
-            //get player title
-            let titleID = data2.Character.Title;
-            $.get(`https://xivapi.com/title/${titleID}`, function(data3, textStatus, jqXQR)
+            let titleID = data2.Character.Title
+            $.when($.ajax(`https://xivapi.com/title/${titleID}`)).done(function(data3,textStatus,jqXQR)
             {
+                //display character name where element ID matches
+                $("#charName").text(data2.Character.Name);
+                //display server name where element ID matches
+                $("#charServer").text(data.Results[0].Server);
+                $("currentClassText").show();
+                $("#currClass").text(data2.Character.ActiveClassJob.UnlockedState.Name)
+                //mini portrait
+                let avaURL = data2.Character.Avatar;
+                //big portrait
+                let portraitURL = data2.Character.Portrait;
+                //job icon
+                jobClass = (data2.Character.ActiveClassJob.UnlockedState.Name).toLowerCase();
+                jobClass = jobClass.replace(" ", '');
+                let jobURL = `https://xivapi.com/cj/companion/${jobClass}.png`;
+                $("#currClassIMG").attr("src",jobURL);
+                //put images in respective tags
+                $("#charProfile").attr("src",avaURL); 
+                $("#charPortrait").attr("src",portraitURL); 
                 //display character title where element ID matches
                 if(data3.IsPrefix===1)
                 {
@@ -55,59 +67,38 @@ const getCharacter = function()
                     $("#charTitleSuffix").text(data3.Name);
 
                 }
-            }
-            );
-            //put images in respective tags
-            $("#charProfile").attr("src",avaURL); 
-            $("#charPortrait").attr("src",portraitURL); 
-            //print data to console
-            console.log(data2);
-            console.log(textStatus);
-            console.log(jqXQR);
-        }
-        );
-        
-        
-    }
-    );
+                //print data to console
+                console.log(data);
+                console.log(data2);
+                console.log(data3);
+                console.log(textStatus);
+                console.log(jqXQR);
+            });
+        })
+    })
 }
 
 
-// //gets a title Name by the ID
-// function getTitle(titleID)
-// {
-//     $.get(`https://xivapi.com/title/${titleID}`, function(data,textStatus,jqXQR)
-//     {
-//         if(data.IsPrefix===1)
-//         {
-//             $("#charTitlePrefix").text(data.Name);
-//             $("#charTitleSuffix").text("");
+let getMount = function()
+{
+    $.get(`https://xivapi.com/mount`)
+    
 
+}
 
-//         }
-//         else if(data.IsPrefix===0)
-//         {
-//             $("#charTitlePrefix").text("");
-//             $("#charTitleSuffix").text(data.Name);
+// POPULATE MY SERVER SELECTION FROM JSON
+function populateList()
+{
+    const selectList = document.getElementById("serverSelect");
+    for(let i = 0; i<servers.length;i++)
+    {
+        var option = document.createElement("option")
+        option.textContent = servers[i];
+        option.value = servers[i];
+        selectList.appendChild(option);
+    }
 
-//         }
-//         console.log(data.Name);
-//     }
-//     );
-// }
-
-
-
-//POPULATE MY SERVER SELECTION FROM JSON
-// function populateList()
-// {
-//     const selectList = document.getElementById("serverSelect");
-//     for()
-//     {
-
-//     }
-
-// }
+}
 
 
 
